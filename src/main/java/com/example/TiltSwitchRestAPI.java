5051a4a3a8747b1555295c4df320785d4bd5c07e
Eugene Bell <eugene_bell@yahoo.com>
2017-03-09 09:17:57 +0000

    added logging and count

--- src/main/java/com/example/TiltSwitchRestAPI.java
+++ src/main/java/com/example/TiltSwitchRestAPI.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,27 +21,34 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TiltSwitchRestAPI {
-
+	
+	private static final Logger LOG = LoggerFactory.getLogger(TiltSwitchRestAPI.class);
+	private int count = 0;
+	
 	@Autowired
 	RabbitTemplate rabbitTemplate; 
 	
-	//http://localhost:8080/get-tilt-switches-by-tiltId/33
+	//http://localhost:8080/get
 	@RequestMapping(value="/get", method=RequestMethod.GET)
 	public String get() {
 		String msg =  (String) rabbitTemplate.receiveAndConvert(DemoApplication.queueName);
 		if (msg != null) {
-			return "got " + msg;
+			LOG.info("Received " + msg);
+			return "Received " + msg;
 		} 
 		return "No messages on queue";
 	}
 	
-	//http://localhost:8080/get-all-tilt-switches
+	//http://localhost:8080/publish/message-1
 	@RequestMapping(value="/publish/{msg}", method=RequestMethod.GET)
-	public String pub(@PathVariable String msg) {	
-		rabbitTemplate.convertAndSend(DemoApplication.exchangeName, "messages", msg);
+	public String pub(@PathVariable String msg) {
+		//for each message I will append a number 
+		String newMessage = count++ + " "+ msg;
+		LOG.info("About to Publish " + msg);
+		rabbitTemplate.convertAndSend(DemoApplication.exchangeName, "messages", newMessage);
+		LOG.info("Published " + msg);
 		return "Published " + msg;
 	}
-	
 
 
 
